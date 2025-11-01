3c338fe24298254e92a037d9c76537fb6a95c951
Jair <JairFolleco29@gmail.com>
2025-10-31 19:52:05 -0500

    update crud

--- crud.py
+++ crud.py
@@ -1,29 +1,33 @@
-from pydantic import BaseModel
-from datetime import datetime
-from typing import List, Optional
+from sqlalchemy.orm import Session
+from models import User, Search
+from schemas import UserCreate, SearchCreate
+from passlib.context import CryptContext
 
-class SearchBase(BaseModel):
-    pokemon: str
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-class SearchCreate(SearchBase):
-    pass
+def get_password_hash(password):
+    return pwd_context.hash(password)
 
-class Search(SearchBase):
-    id: int
-    date: datetime
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
 
-    class Config:
-        orm_mode = True
+def create_user(db: Session, user: UserCreate):
+    hashed_password = get_password_hash(user.password)
+    db_user = User(username=user.username, password_hash=hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
 
-class UserBase(BaseModel):
-    username: str
+def get_user_by_username(db: Session, username: str):
+    return db.query(User).filter(User.username == username).first()
 
-class UserCreate(UserBase):
-    password: str
+def create_search(db: Session, user_id: int, search: SearchCreate):
+    db_search = Search(pokemon=search.pokemon, user_id=user_id)
+    db.add(db_search)
+    db.commit()
+    db.refresh(db_search)
+    return db_search
 
-class User(UserBase):
-    id: int
-    searches: List[Search] = []
-
-    class Config:
-        orm_mode = True
+def get_searches_by_user(db: Session, user_id: int):
+    return db.query(Search).filter(Search.user_id == user_id).order_by(Search.date.desc()).limit(10).all()
